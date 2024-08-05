e6f491ca0dd9b2a1ce8a876e7bc684f16407bca6
John Vilela <johnvvilela@gmail.com>
2024-08-04 23:37:44 -0300

    chore: add supabase middleware config

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,8 @@ import { type NextRequest } from 'next/server';
 import { supabaseMiddleware } from './lib/services/supabase/supabaseMiddleware';
 
 export async function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname === '/') return;
+
   return await supabaseMiddleware(request);
 }
 
